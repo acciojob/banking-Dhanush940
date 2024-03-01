a5dd6ed057d569a318a0f80141f499c25ab280fc
Dhanush940 <dhanushkunamaneni123@gmail.com>
2024-03-01 11:57:15 +0530

    Modified compound interest formula

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -47,7 +47,8 @@ public class SavingsAccount extends BankAccount{
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
-        return getBalance() * (Math.pow((1 + rate/100), (times * years))) - getBalance();
+//        return getBalance() * (Math.pow((1 + rate/100), (times * years))) - getBalance();
+        return getBalance() * Math.pow((1 + rate / (100 * times)), times * years);
     }
 
 }
